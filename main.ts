89459543b7809aa1fe3c7aaed22195660171354c
testdeveloper123356 <ahmadharraz.iman@gmail.com>
2025-04-30 22:36:39 +0800

    fix 002

--- main.ts
+++ main.ts
@@ -564,44 +564,6 @@ directionX = 1
 directionY = 1
 let v8 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
-    . . . . f f f 3 3 3 f f . . . . 
-    . . . f f f 2 3 3 2 3 3 f . . . 
-    . . f f f e 3 3 3 3 e 3 f f . . 
-    . . f f 3 3 2 3 3 3 3 e e f . . 
-    . . f 3 2 f 3 3 3 f 3 2 e f . . 
-    . . 3 3 f f 3 3 3 3 f f f f . . 
-    . f f e 3 3 3 3 3 f b f e f f . 
-    . f e e 4 1 f 3 3 f 1 4 e e f . 
-    . . f e e d d 3 3 d d e e f . . 
-    . . . f e e 4 3 3 4 e e f . . . 
-    . . e 4 f 2 2 3 3 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-controller.player2.moveSprite(v8)
-let list = sprites.create(img`
-    5 5 5 5 5 5 f f f f 5 5 5 5 5 5 
-    5 5 5 5 f f f 2 2 f f f 5 5 5 5 
-    5 5 5 f f f 2 2 2 2 f f f 5 5 5 
-    5 5 f f f e e e e e e f f f 5 5 
-    5 5 f f e 2 2 2 2 2 2 e e f 5 5 
-    5 5 f e 2 f f f f f f 2 e f 5 5 
-    5 5 f f f f e e e e f f f f 5 5 
-    5 f f e f b f 5 5 f b f e f f 5 
-    5 f e e 4 1 f 5 5 f 1 4 e e f 5 
-    5 5 f e e d d 5 5 d d e e f 5 5 
-    5 5 5 f e e 4 4 4 4 e e f 5 5 5 
-    5 5 e 4 f 2 2 2 2 2 2 f 4 e 5 5 
-    5 5 4 d f 2 2 2 2 2 2 f d 4 5 5 
-    5 5 4 4 f 4 4 5 5 4 4 f 4 4 5 5 
-    5 5 5 5 5 f f f f f f 5 5 5 5 5 
-    5 5 5 5 5 f f 5 5 f f 5 5 5 5 5 
-    `, SpriteKind.Player)
-controller.player3.moveSprite(list)
-let kfrjkrjfg = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
     . . . . f f f 8 8 f f f . . . . 
     . . . f f f 8 8 8 8 f f f . . . 
     . . f f f 8 8 8 8 8 8 f f f . . 
@@ -614,6 +576,44 @@ let kfrjkrjfg = sprites.create(img`
     . . . f e e 4 4 4 4 e e f . . . 
     . . e 4 f 8 8 8 8 8 8 f 4 e . . 
     . . 4 d f 8 8 8 8 8 8 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.player2.moveSprite(v8)
+let list = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 5 5 f f f . . . . 
+    . . . f f f 5 5 5 5 f f f . . . 
+    . . f f f 5 5 5 5 5 5 f f f . . 
+    . . f f 5 5 5 5 5 5 5 5 5 f . . 
+    . . f 5 5 f f f f f f 5 5 f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 5 5 5 5 5 5 f 4 e . . 
+    . . 4 d f 5 5 5 5 5 5 f d 4 . . 
+    . . 4 4 f 4 4 2 2 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.player3.moveSprite(list)
+let kfrjkrjfg = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 7 7 f f f . . . . 
+    . . . f f f 7 7 7 7 f f f . . . 
+    . . f f f 7 7 7 7 7 7 f f f . . 
+    . . f f 7 7 7 7 7 7 7 7 7 f . . 
+    . . f 7 7 f f f f f f 7 7 f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 7 7 7 7 7 7 f 4 e . . 
+    . . 4 d f 7 7 7 7 7 7 f d 4 . . 
     . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
